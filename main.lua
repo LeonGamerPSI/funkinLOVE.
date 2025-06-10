ed7051e857cd954c47ebb32d6b80856d870e308f
LeonGamerPSI <fnfmaker666@gmail.com>
2025-06-10 19:51:42 +0200

    fuck off gitfagmodules

--- main.lua
+++ main.lua
@@ -1,15 +1,14 @@
-
+_G["Engine"] = require("dovey.engine")
 function love.load(arg)
-  
+
+
+    Engine.begin({})
+
 end
 
 function love.update(dt)
-  
 end
 
-
 function love.draw()
-
-  love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 720*0.04)
-
+  love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 10, 720 * 0.04)
 end
